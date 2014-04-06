c7acdf35b1b0e29fbe2c74b78b34ae79c4435970
mdiasloureiro <mdiasloureiro@gmail.com>
2014-04-06 23:40:26 +0100

    refactor

--- app/controllers/occurrence.js
+++ app/controllers/occurrence.js
@@ -1,26 +1,48 @@
-// Since we are using the Cordova SQLite plugin, initialize AngularJS only after deviceready
+/* 
+ * Application description
+ * TODO
+ */
+
 document.addEventListener("deviceready", function() {
   angular.bootstrap(document, ['occurrenceApp']);
 });
 
 var occurrenceApp = angular.module('occurrenceApp', ['OccurrenceModel', 'hmTouchevents']);
 
+/*
+ *
+ * Main and only controller atm
+ *
+ */
+
 occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
   // Will rotate to every direction
   steroids.view.setAllowedRotations([0,180,-90,90]);  
   
-  // Current selected route
-  $scope.currentRoute = false;
+  // Route states
+  $scope.currentRoute = null;
+  $scope.currentRouteWatcher = null;
+  $scope.currentSubRoute = null;
+  $scope.currentOccurrence = null;
 
-  // Populated by $scope.loadFromPersistence
+  // Data structures for the application
+  // this structures define the current
+  // state of the application
+  // it will be all saved in localStorage
   $scope.routes = [];
   $scope.currentOccurrences = [];
-  // individual occ
-  //$scope.occ = []; 
-
   $scope.currentMarker = null;
   $scope.currentPolyline = null;
-  // instances state array
+
+  // type of 'pavimentos' for route details
+  $scope.pavimentos = [{'id': '1', 'name' : 'flexivel'},
+                       {'id': '2', 'name' : 'rigido'},
+                       {'id': '3', 'name' : 'semirrigido'},
+                       {'id': '4', 'name' : 'blocos_de_betao'},
+                       {'id': '5', 'name' : 'paralelepidos_de_rocha_natural'},
+                       {'id': '6', 'name' : 'outro'}];
+  
+  // type of occurrences, check with backend
   $scope.instances = {
     '11' : {'name' : 'Rodeiras - Tipo 1'},
     '12' : {'name' : 'Rodeiras - Tipo 2', 'watching' : false, 'points': [], 'watch_id': null},
@@ -38,7 +60,7 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
     '52' : {'name' : 'Reparações - Tipo 2'},
     '53' : {'name' : 'Reparações - Tipo 3'}
   };
-
+  
   // instantiates the map
   var map = L.map('map-container').setView([40.20641, -8.409745], 13);
 
@@ -48,6 +70,10 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
       maxZoom: 18
   }).addTo(map);
 
+  /**
+   * clearLayers function to clear the map
+   * @return void
+   */
   $scope.clearLayers = function() {
     // clear markers if they exist
     if($scope.currentMarker) {
@@ -61,120 +87,141 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
     }
   };
 
-  // START AND STOP EVENT HANDLERS
-  $scope.startRoad = function($event) {
+  /**
+   * startRoute starts a new route enabling the main path
+   * @param  Object event
+   */
+  $scope.startRoute = function($event) {
+    // gets the current selected route
+    var currentRoute = $scope.getCurrentRoute();
+    // starts the watcher 
+    var options = { timeout: 30000, enableHighAccuracy: true };
 
-    if ($scope.currentRoute != false) {
+    $scope.currentRouteWatcher = navigator.geolocation.watchPosition(
+      function(position) {
+        $scope.instances[id].points.push(position);
+      }, 
+      function(error) {
+        alert(error);
+      }, 
+    options);
+  };
+
+  /**
+   * startPathOccurrence init GPS watcher and makes the relation to the instances
+   * @param  int id is the type of instance referenced in $scope.instances
+   */
+  $scope.startPathOccurrence = function(id) {
+    // remove if we have something
+    $scope.clearLayers();
+    // just a flag to check wether we'r watching or not
+    $scope.instances[id].watching = true;
+    // starts the watcher 
+    var options = { timeout: 30000, enableHighAccuracy: true };
+
+    $scope.instances[id].watch_id = navigator.geolocation.watchPosition(
+      function(position) {
+        // remove the last one
+        $scope.clearLayers();
+        var pos = [position.coords.latitude, position.coords.longitude];
+        $scope.currentMarker = L.marker(pos).addTo(map);
+        // for every location update add the point to the 
+        // updated state of the instance object
+        $scope.instances[id].points.push(position);
+      }, 
+      function(error) {
+        alert(error);
+      }, 
+    options);
+  };
+
+  $scope.stopsAndSavePathOccurrence = function(id) {
+    $scope.clearLayers();
+    // updates the flag
+    $scope.instances[id].watching = false;
+    // stop watching
+    navigator.geolocation.clearWatch($scope.instances[id].watch_id);
+    $scope.instances[id].watch_id = null;
+    
+    // draw the line 
+    var path = [];
+    var pathObject = $scope.instances[id].points;
+
+    // get the points from current state of the instance 
+    // creates the array and draws the polyline
+    for(var p in pathObject) {
+      path.push([pathObject[p].coords.latitude, pathObject[p].coords.longitude]);
+    }
+    // save the occurrence
+    $scope.addOccurrence({
+      id : new Date().getTime(),
+      instance_id : id,
+      position : null,
+      path : path,
+      name: $scope.instances[id].name,
+      createddate : new Date(),
+      type: 'path'
+    });
+
+    /* refresh */ 
+    $scope.$apply();
+    $scope.currentPolyline = L.polyline(path, {color: 'red'}).addTo(map);
+    // zoom the map to the polyline
+    map.fitBounds($scope.currentPolyline.getBounds());
+    // clear points  
+    $scope.instances[id].points = [];
+    // stop watching 
+    steroids.view.navigationBar.show("Speroroads :: Gravado " + $scope.instances[id].name);
+  };
+
+  /**
+   * triggerPathOcc function that starts or stops a type path occurrence,
+   *                triggered when a button is clicked
+   * @param Object $event has the details of the event, we need this to check
+   *                      the type of occ it is
+   * @return void changes the state of the app
+   */
+  $scope.triggerPathOcc = function($event) {
+    if ($scope.currentRoute != null) {
       var id = $event.target.attributes.rel.value;
-
-      // if it's watching something stops
+      // if it's watching something, stops
       if($scope.instances[id].watching) {
-
-        $scope.clearLayers();
-
-        //$event.target.attr('class','topcoat-button--large speroroads-bottom');
-
-        // updates the flag
-        $scope.instances[id].watching = false;
-
-        // stop watching
-        navigator.geolocation.clearWatch($scope.instances[id].watch_id);
-        $scope.instances[id].watch_id = null;
-        
-        // draw the line 
-        var path = [];
-        var pathObject = $scope.instances[id].points;
-
-        // get the points from current state of the instance 
-        // creates the array and draws the polyline
-        for(var p in pathObject) {
-          path.push([pathObject[p].coords.latitude, pathObject[p].coords.longitude]);
-        }
-
-        // save the occurrence
-        var occurrence = {
-          id : event.target.attributes.rel.value,
-          position : null,
-          path : path,
-          name: $scope.instances[id].name,
-          createddate : new Date(),
-          type: 'path'
-        }
-
-        $scope.addOccurrence(occurrence);
-
-        /* refresh */ 
-        $scope.$apply();
-
-        $scope.currentPolyline = L.polyline(path, {color: 'red'}).addTo(map);
-        // zoom the map to the polyline
-        map.fitBounds($scope.currentPolyline.getBounds());
-        // clear points  
-        $scope.instances[id].points = [];
-        // stop watching 
-        steroids.view.navigationBar.show("Speroroads :: Gravado " + $scope.instances[id].name);
-
+        $scope.stopsAndSavePathOccurrence(id);
       } else {
-        // remove if we have something
-        $scope.clearLayers();
-
-        // just a flag to check wether we'r watching or not
-        $scope.instances[id].watching = true;
-
-        // starts the watcher 
-        var options = { timeout: 30000, enableHighAccuracy: true };
-        
-        $scope.instances[id].watch_id = navigator.geolocation.watchPosition(
-          function(position) {
-            // remove the last one
-            $scope.clearLayers();
-
-            var pos = [position.coords.latitude, position.coords.longitude];
-            $scope.currentMarker = L.marker(pos).addTo(map);
-
-            // for every location update add the point to the 
-            // updated state of the instance object
-
-            $scope.instances[id].points.push(position);
-          }, 
-          function(error) {
-            alert(error);
-          }, 
-          options);
-        steroids.view.navigationBar.show("Speroroads :: Localizando...");
+        $scope.startPathOccurrence(id);
       }
     } else {
       alert('You need to select or create a new route to add occurrences.');
     }
   };
 
-  /* SINGLE POINT INSTANCE */ 
-
-  $scope.save = function($event) {
+  /**
+   * save When it's a single type occurrence we can save it instantly
+   * @param  Object $event same as above
+   * @return void change the state of the app
+   */
+  $scope.saveSingleOccurrence = function($event) {
     if ($scope.currentRoute != false) {
       navigator.geolocation.getCurrentPosition(function(position) {  
         // clear markers if they exist
         $scope.clearLayers();
-
-        var occurrence = {
-          id : $event.target.attributes.rel.value,
-          position : position,
-          path : null,
-          createddate : new Date(),
-          type: 'single'
-        }
 
         var pos = [position.coords.latitude, position.coords.longitude];
         /* create layer to easily remove marker */
         $scope.currentMarker = L.marker(pos).addTo(map);
 
         //$scope.occ.push(occurrence);
-        $scope.addOccurrence(occurrence);
-        //$scope.currentOccurrences = $scope.getCurrentRoute().occurrences;
-
+        $scope.addOccurrence({
+          id : new Date().getTime(),
+          instance_id : $event.target.attributes.rel.value,
+          position : position,
+          path : null,
+          createddate : new Date(),
+          type: 'single',
+        });
+        
         /* refresh */ 
         $scope.$apply();
-
         steroids.view.navigationBar.show("Speroroads :: Gravado " + $scope.instances[$event.target.attributes.rel.value].name);
       }, 
       function(error) {
@@ -188,52 +235,46 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
   $scope.renderRoute = function(route) {
     $scope.currentRoute = route.id;
     $scope.currentOccurrences = route.occurrences;
-    $scope.routeDetails = routeDetails;
+    //$scope.routeDetails = routeDetails;
   },
 
+  /**
+   * newRoute creates add's a new route to the current state of the app
+   * @return void 
+   */
   $scope.newRoute = function() {
-
-    var route_id = $scope.routes.length + 1;
-
+    var dt = new Date();
+    var route_id = dt;
+    var buildDate = dt.getDate() + '/'+ dt.getMonth() + '/' + dt.getFullYear() + ' ' + dt.getHours() + ':' + dt.getMinutes();
     var route = {
-      id: route_id,
-      name: "Name #"+route_id,
+      id: dt.getTime(),
+      name: buildDate,
+      subRoutes : [],
       occurrences: [],
       options: {
-        pavimento: 'Tipo X',
-        separador: false,
-        bermas_pavimentadas: true,
-        largura_berma: 0.2,
-        n_vias: 3,
-        largura_total_pavimentada: 2.4,
-        largura_pavimentada_sentidos: [2.5, 5.6]
       }
     }
-
     $scope.routes.push(route);
-
-    document.getElementById('routeoptions').innerHTML = "";
-    document.getElementById('routeoptions').innerHTML += "<div class='col-md-8'><strong>Pavimento</strong></div><div class='col-md-4'>"+route['options']['pavimento']+"</div>";
-    document.getElementById('routeoptions').innerHTML += "<div class='col-md-8'><strong>Bermas pavimentadas</strong></div><div class='col-md-4'>"+route['options']['bermas_pavimentadas']+"</div>";
-    document.getElementById('routeoptions').innerHTML += "<div class='col-md-8'><strong>Largura da berma</strong></div><div class='col-md-4'>"+route['options']['largura_berma']+"</div>";
-    document.getElementById('routeoptions').innerHTML += "<div class='col-md-8'><strong>Número de vias</strong></div><div class='col-md-4'>"+route['options']['n_vias']+"</div>";
-    document.getElementById('routeoptions').innerHTML += "<div class='col-md-8'><strong>Largura total pavimentada</strong></div><div class='col-md-4'>"+route['options']['largura_total_pavimentada']+"</div>";
-
-    /* MADNESS */
-    /* TO DELETE AND UPDATE */
     $scope.currentRoute = route_id;
     $scope.currentOccurrences.length = 0;
     $scope.currentOccurrences = [];
-    //$scope.currentOccurrences = route.occurrences;
-
   };
 
+  /**
+   * saveRoute saves the current state of the app to localStorage
+   * @param Object $event 
+   * @return void
+   */
   $scope.saveRoute = function($event) {
     $scope.currentOccurrences.length = 0;
     $scope.currentOccurrences = [];
     $scope.saveToPersistent();
   },
 
+  /**
+   * addOccurrence to current route data structure
+   * @param Object occurrence
+   */
   $scope.addOccurrence = function(occurrence) {
     for (var i = 0; i < $scope.routes.length; i++) {
       if($scope.routes[i].id == $scope.currentRoute) {
@@ -245,6 +286,10 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
     return false;
   },
 
+  /**
+   * getCurrentRoute helper function to check get the currentRoute object
+   * @return Object the actual route
+   */
   $scope.getCurrentRoute = function() {
     for (var i = 0; i < $scope.routes.length; i++) {
       if($scope.routes[i].route_id == $scope.currentRoute) {
@@ -254,6 +299,10 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
     return false;
   },
 
+  /**
+   * openOccurrence Displays the occurrence info, and shows it on the map
+   * @param  int id the occurrence id
+   */
   $scope.openOccurrence = function(id) {
     
     $scope.clearLayers();
@@ -262,6 +311,9 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
     
     for(var o in $scope.currentOccurrences) {
       if(parseInt($scope.currentOccurrences[o].id) == parseInt(id)) {
+
+        $scope.currentOccurrence = $scope.currentOccurrences[o];
+
         // check the type
         if($scope.currentOccurrences[o].type == 'single') {
           pos = $scope.currentOccurrences[o].position;
@@ -270,11 +322,9 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
         } else {
 
           var path = $scope.currentOccurrences[o].path;
-          
           $scope.currentPolyline = L.polyline(path, {color: 'red'}).addTo(map);
           // zoom the map to the polyline
           map.fitBounds($scope.currentPolyline.getBounds());
-
         }
         steroids.view.navigationBar.show("Speroroads :: Vendo " + $scope.instances[$scope.currentOccurrences[o].id].name);
       }
@@ -287,8 +337,12 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
   };
 
   $scope.loadFromPersistent = function(id) {
-    $scope.routes = JSON.parse(localStorage.getItem('routes'));
-    if ($scope.routes == null) {
+    try {
+      $scope.routes = JSON.parse(localStorage.getItem('routes'));
+      if ($scope.routes == null) {
+        $scope.routes = [];
+      }
+    } catch(e) {
       $scope.routes = [];
     }
   };
@@ -308,19 +362,17 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
 
   };
 
-  /* Clear Occurrences */
-  /* TO BE DELETED */
-  /*
-  $scope.clearOccurrences = function() {
-    $scope.occ.length = 0;
-    $scope.occ = [];
-  },*/
-
+  /**
+   * clearPersistent clear all localStorage objects
+   */
   $scope.clearPersistent = function() {
     localStorage.clear();
   };
 
-  // Helper function for opening new webviews
+  /**
+   * open Route
+   * @param  int id route ID
+   */
   $scope.open = function(id) {
     for (var i = 0; i < $scope.routes.length; i++) {
       if($scope.routes[i].id == id) {
@@ -330,54 +382,10 @@ occurrenceApp.controller('IndexCtrl', function ($scope, Occurrence) {
     };
   };
 
-  /* START AND STOP EVENT HANDLERS */ 
-
-  /*$scope.loadOccurrences = function() {
-    $scope.loading = true;
-
-    persistence.clean();  // Clean persistence.js cache before making a query
-
-    // Persistence.js query for all occurrences in the database
-    Occurrence.all().list(function(occurrences) {
-      $scope.occurrences = occurrences;
-      $scope.loading = false;
-      $scope.$apply();
-    });
-  };*/
-
-  // Fetch all objects from the backend (see app/models/occurrence.js)
-  //$scope.loadOccurrences();
-  
   // Fetch all objects from LocalStorage
   $scope.loadFromPersistent();
 
-  // Get notified when an another webview modifies the data and reload
-  window.addEventListener("message", function(event) {
-    // reload data on message with reload status
-    if (event.data.status === "reload") {
-      //$scope.loadOccurrences();
-      $scope.loadFromPersistent();
-    };
-  });
-
-  // -- Native navigation
-
   // Set up the navigation bar
   steroids.view.navigationBar.show("Prototype");
-
-  // Define a button for adding a new occurrence
-  var addButton = new steroids.buttons.NavigationBarButton();
-  addButton.title = "Add";
-
-  // Set a callback for the button's tap action...
-  addButton.onTap = function() {
-    var addView = new steroids.views.WebView("/views/occurrence/new.html");
-    steroids.modal.show(addView);
-  };
-
-  // ...and finally show it on the navigation bar.
-  steroids.view.navigationBar.setButtons({
-    right: [addButton]
-  });
 
 });
